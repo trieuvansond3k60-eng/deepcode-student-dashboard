d1f988e7812df30f62c9aaccb7f7ec2241161c45
trieuvanson <trieuvansond3k60@gmail.com>
2026-09-26 16:08:38 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
     <BrowserRouter>
       {/* Your app content */}
        <Routes>
-        <Route path="/" element={HomePage } />
-        <Route path="/student" element={ StudentPage} />
+        <Route path="/" element={<HomePage/> } />
+        <Route path="/student" element={ <StudentPage/>} />
         <Route path="/course" element={<CoursePage />} />
         <Route path="/setting" element={<SettingPage />} />
       </Routes>
