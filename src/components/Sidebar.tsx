38c5d05fe3fb66af72db165a1fc6e52dc654d066
trieuvanson <trieuvansond3k60@gmail.com>
2026-09-26 16:03:25 +0700

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,25 +1,33 @@
 import { IoMdHome, IoMdPerson, IoIosSettings } from "react-icons/io";
 import { FaBookReader } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className= "w-64 flex-col h-screen bg-gray-800 text-white p-4 flex">
-        <div className="flex items-center mb-6">
-            <IoMdHome className="mr-2" />
-            <span>Trang chủ</span>
-        </div>
-        <div className="flex items-center mb-6">
-            <IoMdPerson className="mr-2" />
-            <span>Quản lý sinh viên</span>
-        </div>
-        <div className="flex items-center mb-6">
-            <FaBookReader className="mr-2" />
-            <span>Khóa học</span>
-        </div>
-        <div className="flex items-center">
-            <IoIosSettings className="mr-2" />
-            <span>Cài đặt</span>
-        </div>
+    <div className="w-64 flex-col h-screen bg-gray-800 text-white p-4 flex">
+      
+      <div className="flex items-center mb-6">
+        <IoMdHome className="mr-2" />
+        <span>Trang chủ</span>
+      </div>
+
+      <Link
+        to="/student"
+        className="flex items-center mb-6 hover:bg-gray-700 p-2 rounded"
+      >
+        <IoMdPerson className="mr-2" />
+        <span>Quản lý sinh viên</span>
+      </Link>
+
+      <div className="flex items-center mb-6">
+        <FaBookReader className="mr-2" />
+        <span>Khóa học</span>
+      </div>
+
+      <div className="flex items-center">
+        <IoIosSettings className="mr-2" />
+        <span>Cài đặt</span>
+      </div>
     </div>
   );
 };
